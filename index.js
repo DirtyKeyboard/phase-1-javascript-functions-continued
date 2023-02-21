7ce3a7121323193fd4958d19698e7dd2fdc2a218
Andrew Hawileh <andrewh829@gmail.com>
2023-02-21 17:26:41 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun (act)
+{
+    if (act==undefined)
+         act="roller-skate"
+
+    return "This Saturday, I want to " + act +"!";
+}
+
+function mondayWork(act)
+{
+    if (act==undefined)
+         act="go to the office"
+
+    return "This Monday, I will " + act +".";
+}
+
+//Must return a function
+function wrapAdjective(wrap)
+{
+    const wrapAdj = (adj) => "You are " + wrap+adj+wrap + "!"; 
+    return wrapAdj;
+}
